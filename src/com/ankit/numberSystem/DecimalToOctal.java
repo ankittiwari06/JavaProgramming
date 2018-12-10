9b40af64feb9d2a943258cce34b14cbd39a9d433
ankittiwari06 <ankittiwari0693@gmail.com>
2018-12-10 13:43:31 +0530

    DecimalToOctalConversion

--- src/com/ankit/numberSystem/DecimalToOctal.java
+++ src/com/ankit/numberSystem/DecimalToOctal.java
@@ -9,26 +9,24 @@ import java.util.Scanner;
  * @author ankit
  *
  */
-public class DecimalToBinary {
+public class DecimalToOctal {
 
 	/**
 	 * @param args
 	 */
 
-	public static void binaryNumber(int dec_num) {
-		int bin_num[] = new int[100];
-		int rem, quot, i = 1;
+	public static void decimalToOctal(int dec_num) {
+		int quot, i = 1;
+		int oct_num[] = new int[100];
 		quot = dec_num;
-
 		while (quot != 0) {
-			bin_num[i++] = quot % 2;
-			quot = quot / 2;
+			oct_num[i++] = quot % 8;
+			quot = quot / 8;
 		}
-
 		for (int j = i - 1; j > 0; j--) {
-			System.out.print(bin_num[j]);
+			System.out.print(oct_num[j]);
 		}
-		System.out.println("\n");
+		System.out.print("\n");
 	}
 
 	public static void main(String[] args) {
@@ -36,8 +34,7 @@ public class DecimalToBinary {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter Decimal Number : ");
 		int dec_num = sc.nextInt();
-		System.out.print("Binary Number is ");
-		binaryNumber(dec_num);
+		System.out.print("Octal value is : ");
+		decimalToOctal(dec_num);
 	}
-
 }
